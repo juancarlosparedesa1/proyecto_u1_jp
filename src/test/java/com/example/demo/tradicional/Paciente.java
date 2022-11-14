24e03ad90ca65f62a90e4d933f125973ddf88d03
juancarlosparedesa1 <jcparedesa1@uce.edu.ec>
2022-11-14 12:26:21 -0500

    Mi segundo proyecto

---
+++ src/test/java/com/example/demo/tradicional/Paciente.java
@@ -0,0 +1,21 @@
+package com.example.demo.tradicional;
+
+public class Paciente {
+	private String nombre;
+	private String cedula;
+	
+	//SET Y GET
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}	
+
+}
